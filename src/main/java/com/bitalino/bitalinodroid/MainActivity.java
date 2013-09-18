5bddb34ec5b0c5a53b717cd18d6690bdccb9bd46
Paulo Pires <pjpires@gmail.com>
2013-09-18 16:41:58 +0100

    Improved output.
    Signed-off-by: Paulo Pires <pjpires@gmail.com>

--- src/main/java/com/bitalino/bitalinodroid/MainActivity.java
+++ src/main/java/com/bitalino/bitalinodroid/MainActivity.java
@@ -4,15 +4,15 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.bitalino.BITalinoDevice;
-import com.bitalino.BITalinoException;
 import com.bitalino.BITalinoFrame;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.UUID;
@@ -31,10 +31,6 @@ public class MainActivity extends Activity {
    */
   private static final UUID MY_UUID = UUID
           .fromString("00001101-0000-1000-8000-00805F9B34FB");
-  private BluetoothDevice dev = null;
-  private BluetoothSocket sock = null;
-  private InputStream is = null;
-  private OutputStream os = null;
   private boolean testInitiated = false;
 
   @Override
@@ -43,11 +39,7 @@ public class MainActivity extends Activity {
     setContentView(R.layout.activity_main);
 
     if (!testInitiated)
-      try {
-        test();
-      } catch (Exception e) {
-        Log.e(TAG, "EXCEPTION: ", e);
-      }
+      new TestAsyncTask().execute();
   }
 
   @Override
@@ -57,12 +49,21 @@ public class MainActivity extends Activity {
     return true;
   }
 
-  void test() throws IOException, BITalinoException {
-    // Let's get the remote Bluetooth device
-    final String remoteDevice = "20:13:08:08:15:83";
+  private class TestAsyncTask extends AsyncTask<Void, String, Void> {
+    private TextView tvLog = (TextView) findViewById(R.id.log);
+    private BluetoothDevice dev = null;
+    private BluetoothSocket sock = null;
+    private InputStream is = null;
+    private OutputStream os = null;
 
-    final BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
-    dev = btAdapter.getRemoteDevice(remoteDevice);
+    @Override
+    protected Void doInBackground(Void... paramses) {
+      try {
+        // Let's get the remote Bluetooth device
+        final String remoteDevice = "20:13:08:08:15:83";
+
+        final BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        dev = btAdapter.getRemoteDevice(remoteDevice);
 
     /*
      * Establish Bluetooth connection
@@ -78,37 +79,53 @@ public class MainActivity extends Activity {
      * http://developer.android.com/reference/android/bluetooth/BluetoothAdapter
      * .html#cancelDiscovery()
      */
-    Log.d(TAG, "Stopping Bluetooth discovery.");
-    btAdapter.cancelDiscovery();
+        Log.d(TAG, "Stopping Bluetooth discovery.");
+        btAdapter.cancelDiscovery();
 
-    sock = dev.createRfcommSocketToServiceRecord(MY_UUID);
-    sock.connect();
-    testInitiated = true;
+        sock = dev.createRfcommSocketToServiceRecord(MY_UUID);
+        sock.connect();
+        testInitiated = true;
 
-    BITalinoDevice bitalino = new BITalinoDevice(10, new int[]{0});
-    bitalino.open(sock.getInputStream(), sock.getOutputStream());
+        BITalinoDevice bitalino = new BITalinoDevice(10, new int[]{0});
+        publishProgress("Connecting to BITalino [" + remoteDevice + "]..");
+        bitalino.open(sock.getInputStream(), sock.getOutputStream());
+        publishProgress("Connected.");
 
-    // get BITalino version
-    Log.i(TAG, "VERSION: " + bitalino.version());
+        // get BITalino version
+        publishProgress("Version: " + bitalino.version());
 
-    // start acquisition on predefined analog channels
-    bitalino.start();
+        // start acquisition on predefined analog channels
+        bitalino.start();
 
-    // read n samples
-    final int numberOfSamplesToRead = 10;
-    Log.i(TAG, "Reading " + numberOfSamplesToRead + " samples..");
-    BITalinoFrame[] frames = bitalino.read(numberOfSamplesToRead);
-    for (BITalinoFrame frame : frames)
-      Log.d(TAG, "FRAME: " + frame.toString());
+        // read n samples
+        final int numberOfSamplesToRead = 10;
+        publishProgress("Reading " + numberOfSamplesToRead + " samples..");
+        BITalinoFrame[] frames = bitalino.read(numberOfSamplesToRead);
+        for (BITalinoFrame frame : frames)
+          publishProgress(frame.toString());
 
-    // trigger digital outputs
-    // int[] digital = { 1, 1, 1, 1 };
-    // device.trigger(digital);
+        // trigger digital outputs
+        // int[] digital = { 1, 1, 1, 1 };
+        // device.trigger(digital);
 
-    // stop acquisition and close bluetooth connection
-    bitalino.stop();
+        // stop acquisition and close bluetooth connection
+        bitalino.stop();
+        publishProgress("BITalino is stopped");
 
-    sock.close();
+        sock.close();
+        publishProgress("And we're done! :-)");
+      } catch (Exception e) {
+        Log.e(TAG, "There was an error.", e);
+      }
+
+      return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(String... values) {
+      tvLog.append("\n".concat(values[0]));
+    }
+
   }
 
 }
